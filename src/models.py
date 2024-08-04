2604a1d0f67faa0383ba9ae303ddce75309c0b89
fschwanck <fschwanck@gmail.com>
2024-08-04 19:55:08 -0300

    committing

--- src/models.py
+++ src/models.py
@@ -3,6 +3,7 @@ from os import makedirs, path
 import requests
 import bz2
 from src.logging import get_logger
+import tensorflow as tf
 
 log = get_logger('models.py')
 class ShapePredictor:
@@ -47,3 +48,63 @@ class ShapePredictor:
         else:
             log.error(f"Failed to load model: {ShapePredictor.__dat_path}")
             raise FileNotFoundError(f"Could not find file: {ShapePredictor.__dat_path}")
+
+import tensorflow as tf
+
+# Ensure TensorFlow 2.17.0 is used
+assert tf.__version__ == '2.17.0', f"Expected TensorFlow version 2.17.0 but got {tf.__version__}"
+
+# Function to create the Learnable Multiplication Kernel (LMK) layer
+class LMKLayer(tf.keras.layers.Layer):
+    def __init__(self, filters, kernel_size, **kwargs):
+        super(LMKLayer, self).__init__(**kwargs)
+        self.filters = filters
+        self.kernel_size = kernel_size
+
+    def build(self, input_shape):
+        self.kernel = self.add_weight(
+            shape=self.kernel_size,
+            initializer='truncated_normal',
+            trainable=True,
+            name='kernel'
+        )
+        self.bias = self.add_weight(
+            shape=[self.filters],
+            initializer='zeros',
+            trainable=True,
+            name='bias'
+        )
+
+    def call(self, inputs):
+        conv = tf.nn.conv2d(inputs, self.kernel, strides=[1, 1, 1, 1], padding='SAME')
+        conv = tf.nn.bias_add(conv, self.bias)
+        return conv
+
+# Define the Basic-FreNet model using tf.keras.Model
+class BasicFreNetModel(tf.keras.Model):
+    def __init__(self, n_layers, k_values, conv_kernel_size, filters_summary, **kwargs):
+        super(BasicFreNetModel, self).__init__(**kwargs)
+        self.n_layers = n_layers
+        self.lmk_layers = [LMKLayer(filters=f, kernel_size=conv_kernel_size) for f in k_values]
+        self.summary_lmk_layer = LMKLayer(filters=filters_summary, kernel_size=conv_kernel_size)
+        self.pool = tf.keras.layers.MaxPooling2D(pool_size=[2, 2], strides=2, padding='SAME')
+        self.flatten = tf.keras.layers.Flatten()
+        self.fc1 = tf.keras.layers.Dense(units=2048, activation='relu')
+        self.dropout1 = tf.keras.layers.Dropout(0.5)
+        self.fc2 = tf.keras.layers.Dense(units=512, activation='relu')
+        self.dropout2 = tf.keras.layers.Dropout(0.5)
+        self.output_layer = tf.keras.layers.Dense(units=6, activation='softmax')
+
+    def call(self, inputs):
+        x = inputs
+        for lmk_layer in self.lmk_layers:
+            x = lmk_layer(x)
+        x = self.summary_lmk_layer(x)
+        x = self.pool(x)
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.dropout1(x)
+        x = self.fc2(x)
+        x = self.dropout2(x)
+        x = self.output_layer(x)
+        return x
